d89ede66b7696fa51e501de5d46ad1d593ef5a92
sarimeh <kingmemo@gmail.com>
2017-08-09 23:00:05 +0200

    -

--- scopetest.py
+++ scopetest.py
@@ -17,6 +17,6 @@ def scope_test():
     print("After nonlocal assignment:", spam)
     do_global()
     print("After global assignment:", spam)
-
+dddd
 scope_test()
 print("In global scope:", spam)
