dcb2d4660af310a364b95ded99cda5d5df4ded11
sarimeh <kingmemo@gmail.com>
2017-10-11 22:17:49 +0200

    made changes

--- mygitubuntu.sh
+++ mygitubuntu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-cd /home/memo/AI
-git pull
-git add .
-git commit -am "made changes"
+cd /home/memo/AI/pythonproj
+sudo git pull
+git add *
+sudo git commit -m "made changes"
 git push origin master
